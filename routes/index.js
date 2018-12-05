188630de6ae10850a55ae5220ec99a13223b9849
Jules Gribble <gribblej@me.com>
2018-12-05 00:52:49 +0000

    build.js

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const path = require('path');
 
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', (req, res, next) => {
+router.get('/', (req, res) => {
   res.render('index', { title: 'Devs in Berkshire' });
+});
+
+router.get('/js/build.js', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/js/build.js'));
 });
 
 router.use((req, res) => {
