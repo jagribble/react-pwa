0e79af165544fc70b9f35b69808d6d508f941625
Jules Gribble <gribblej@me.com>
2018-12-05 01:11:09 +0000

    route

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,12 @@
 const express = require('express');
-const path = require('path');
+
 
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', (req, res) => {
+router.use('/', (req, res) => {
   res.render('index', { title: 'Devs in Berkshire' });
 });
 
-
-router.use((req, res, next) => {
-  if (req.originalUrl.includes('/js/')) {
-    next();
-  }
-  res.render('index', { title: 'Devs in Berkshire' });
-});
 
 module.exports = router;
